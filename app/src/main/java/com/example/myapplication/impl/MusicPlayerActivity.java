04440d406642ca25e9924d2f8f94f7c53c21374e
panagiotislam <plampro21@gmail.com>
2020-05-25 19:25:27 +0300

    Trying Music Player

--- app/src/main/java/com/example/myapplication/impl/MusicPlayerActivity.java
+++ app/src/main/java/com/example/myapplication/impl/MusicPlayerActivity.java
@@ -52,74 +52,75 @@ public class MusicPlayerActivity extends AppCompatActivity {
         asyncTask.execute(artist_song);
 
         //player
-        mp = MediaPlayer.create(this, R.raw.Revelations);
+        mp = MediaPlayer.create(this, R.raw.revelations);
         mp.setLooping(true);
-        mp.seekTo(0);
-        mp.setVolume(0.5f, 0.5f);
+//        mp.seekTo(0);
+//        mp.setVolume(0.5f, 0.5f);
         totalTime = mp.getDuration();
+        mp.start();
 
         // Position Bar
-        positionBar = (SeekBar) findViewById(R.id.positionBar);
-        positionBar.setMax(totalTime);
-        positionBar.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        if (fromUser) {
-                            mp.seekTo(progress);
-                            positionBar.setProgress(progress);
-                        }
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-
-                    }
-                }
-        );
+//        positionBar = (SeekBar) findViewById(R.id.positionBar);
+//        positionBar.setMax(totalTime);
+//        positionBar.setOnSeekBarChangeListener(
+//                new SeekBar.OnSeekBarChangeListener() {
+//                    @Override
+//                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                        if (fromUser) {
+//                            mp.seekTo(progress);
+//                            positionBar.setProgress(progress);
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//                    }
+//                }
+//        );
 
 
         // Volume Bar
-        volumeBar = (SeekBar) findViewById(R.id.volumeBar);
-        volumeBar.setOnSeekBarChangeListener(
-                new SeekBar.OnSeekBarChangeListener() {
-                    @Override
-                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                        float volumeNum = progress / 100f;
-                        mp.setVolume(volumeNum, volumeNum);
-                    }
-
-                    @Override
-                    public void onStartTrackingTouch(SeekBar seekBar) {
-
-                    }
-
-                    @Override
-                    public void onStopTrackingTouch(SeekBar seekBar) {
-
-                    }
-                }
-        );
+//        volumeBar = (SeekBar) findViewById(R.id.volumeBar);
+//        volumeBar.setOnSeekBarChangeListener(
+//                new SeekBar.OnSeekBarChangeListener() {
+//                    @Override
+//                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+//                        float volumeNum = progress / 100f;
+//                        mp.setVolume(volumeNum, volumeNum);
+//                    }
+//
+//                    @Override
+//                    public void onStartTrackingTouch(SeekBar seekBar) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onStopTrackingTouch(SeekBar seekBar) {
+//
+//                    }
+//                }
+//        );
 
         // Thread (Update positionBar & timeLabel)
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (mp != null) {
-                    try {
-                        Message msg = new Message();
-                        msg.what = mp.getCurrentPosition();
-                        handler.sendMessage(msg);
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {}
-                }
-            }
-        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                while (mp != null) {
+//                    try {
+//                        Message msg = new Message();
+//                        msg.what = mp.getCurrentPosition();
+//                        handler.sendMessage(msg);
+//                        Thread.sleep(1000);
+//                    } catch (InterruptedException e) {}
+//                }
+//            }
+//        }).start();
 
 
 
@@ -173,47 +174,47 @@ public class MusicPlayerActivity extends AppCompatActivity {
         }
     }
 
-    private Handler handler = new Handler() {
-
-        @Override
-        public void handleMessage(Message msg) {
-            int currentPosition = msg.what;
-            // Update positionBar.
-            positionBar.setProgress(currentPosition);
-
-            // Update Labels.
-            String elapsedTime = createTimeLabel(currentPosition);
-            elapsedTimeLabel.setText(elapsedTime);
-
-            String remainingTime = createTimeLabel(totalTime-currentPosition);
-            remainingTimeLabel.setText("- " + remainingTime);
-        }
-    };
-
-    public String createTimeLabel(int time) {
-        String timeLabel = "";
-        int min = time / 1000 / 60;
-        int sec = time / 1000 % 60;
-
-        timeLabel = min + ":";
-        if (sec < 10) timeLabel += "0";
-        timeLabel += sec;
-
-        return timeLabel;
-    }
-
-    public void playBtnClick(View view) {
-
-        if (!mp.isPlaying()) {
-            // Stopping
-            mp.start();
-            playBtn.setBackgroundResource(R.drawable.stop);
-
-        } else {
-            // Playing
-            mp.pause();
-            playBtn.setBackgroundResource(R.drawable.play);
-        }
-
-    }
+//    private Handler handler = new Handler() {
+//
+//        @Override
+//        public void handleMessage(Message msg) {
+//            int currentPosition = msg.what;
+//            // Update positionBar.
+//            positionBar.setProgress(currentPosition);
+//
+//            // Update Labels.
+//            String elapsedTime = createTimeLabel(currentPosition);
+//            elapsedTimeLabel.setText(elapsedTime);
+//
+//            String remainingTime = createTimeLabel(totalTime-currentPosition);
+//            remainingTimeLabel.setText("- " + remainingTime);
+//        }
+//    };
+//
+//    public String createTimeLabel(int time) {
+//        String timeLabel = "";
+//        int min = time / 1000 / 60;
+//        int sec = time / 1000 % 60;
+//
+//        timeLabel = min + ":";
+//        if (sec < 10) timeLabel += "0";
+//        timeLabel += sec;
+//
+//        return timeLabel;
+//    }
+//
+//    public void playBtnClick(View view) {
+//
+//        if (!mp.isPlaying()) {
+//            // Stopping
+//            mp.start();
+//            playBtn.setBackgroundResource(R.drawable.stop);
+//
+//        } else {
+//            // Playing
+//            mp.pause();
+//            playBtn.setBackgroundResource(R.drawable.play);
+//        }
+//
+//    }
 }
