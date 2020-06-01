026042a8b144b5a4f871b2128d8a5c58108c8913
panagiotislam <plampro21@gmail.com>
2020-06-01 15:55:48 +0300

    Trying Music Player 2

--- app/src/main/java/com/example/myapplication/SubscriberNode.java
+++ app/src/main/java/com/example/myapplication/SubscriberNode.java
@@ -117,6 +117,8 @@ public class SubscriberNode {
             return new Value();
         }
     }
+
+
 }
 
 
--- app/src/main/java/com/example/myapplication/impl/MusicFile.java
+++ app/src/main/java/com/example/myapplication/impl/MusicFile.java
@@ -15,24 +15,6 @@ public class MusicFile  {
         private String genre;
         private String musicFileExtract;
 
-        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-        public MusicFile(File file, int k) throws InvalidDataException, IOException, UnsupportedTagException {
-            Mp3File mp3 = new Mp3File(file);
-            if(k==1){
-                ID3v1 id3v1Tag = mp3.getId3v1Tag();
-                this.trackName = id3v1Tag.getTitle();
-                this.artistName = id3v1Tag.getArtist();
-                this.genre = id3v1Tag.getGenreDescription();
-                this.albumInfo = id3v1Tag.getAlbum();
-            }else{
-                ID3v2 id3v2Tag = mp3.getId3v2Tag();
-                this.trackName = id3v2Tag.getTitle();
-                this.artistName = id3v2Tag.getArtist();
-                this.genre = id3v2Tag.getGenreDescription();
-                this.albumInfo = id3v2Tag.getAlbum();
-            }
-            this.musicFileExtract = Chunks(file, trackName);
-        }
 
     public MusicFile(String trackName,String artistName,String albumInfo,String genre, String musicFileExtract ){
 
@@ -80,35 +62,6 @@ public class MusicFile  {
 
         public void setMusicFileExtract(String newMusicFileExtract) {
             this.musicFileExtract = newMusicFileExtract;
-        }
-
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    public String Chunks(File file, String Name){
-            int partCounterName = 1;//Name parts from 001, 002, 003, ...
-
-            int sizeOfChunks = 512 * 1024;// 512KB
-            byte[] buffer = new byte[sizeOfChunks];
-            String fileName = Name;
-
-            //try-with-resources to ensure closing stream
-            try (FileInputStream fIs = new FileInputStream(file);
-                 BufferedInputStream bIs = new BufferedInputStream(fIs)) {
-
-                int NumberofBytes = 0;
-                while ((NumberofBytes = bIs.read(buffer)) > 1) {
-                    //Write splitted chunks in new files with a identifier name.
-                    String filePartName = String.format("%s_%03d.mp3", fileName, partCounterName++);
-                    File newFile = new File(file.getParent(), filePartName);
-                    try (FileOutputStream out = new FileOutputStream(newFile)) {
-                        out.write(buffer, 0, NumberofBytes);
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String encoded = Base64.getEncoder().encodeToString(buffer);
-            return encoded;
         }
 
     @Override
